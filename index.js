906567aff6db3d86285cf26dfff0f1921a6e26c5
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-30 17:05:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').value
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, (data) =>{
+    $('#results').html(showRepositories(data))
+  }).fail(displayError())
+}
+
+function showRepositories(data) {
+
+}
